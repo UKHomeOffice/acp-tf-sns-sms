7fb9ec8acef51770c7a0b2035f0ca1162f91ffba
Adam Smith <adam.smith@appvia.io>
2020-08-06 11:39:17 +0100

    Rename resources

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = ">= 0.12"
 }
 
-resource "aws_kms_key" "kms_key" {
-  description             = "KMS key 1"
+resource "aws_kms_key" "sns_kms_key" {
+  description             = "KMS key for ${var.name} sns to sms channel"
   deletion_window_in_days = 10
 }
 
-resource "aws_kms_alias" "kms_key_alias" {
+resource "aws_kms_alias" "sns_kms_key_alias" {
   name          = "alias/${name}-sns-sms-kms"
   target_key_id = aws_kms_key.kms_key.key_id
 }
